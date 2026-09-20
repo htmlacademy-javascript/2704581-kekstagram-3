efe020cf56566e7cf10003edbbfa235e24479158
Elvira Shakirova <elvira.uxdesigner@gmail.com>
2026-09-20 19:13:41 +0500

    Добавляет загрузку фото в превью и в фильтры

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import './utils.js';
-import {renderPosts} from './post-render.js';
 import './upload-image-modal.js';
+import './image-upload-preview.js';
+
 import {getData} from './api.js';
+import {renderPosts} from './post-render.js';
 import {showDataError} from './form-notification.js';
 import {showFilters, initFilters } from './image-filters.js';
 
